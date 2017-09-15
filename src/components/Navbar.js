b6f17e85f3508ac013ce98699f381f757d4048c9
angus.chen[陳志源] <angus.chen@104.com.tw>
2017-09-15 18:10:05 +0800

    eject setting & fix warnning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
-import React, {component} from 'react'
+import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => (
   <ul>
-    <li><a href="#">button1</a></li>
-    <li><a href="#">button2</a></li>
-    <li><a href="#">button3</a></li>
-    <li><a href="#">button4</a></li>
+    <li><a href="https://google.com">button1</a></li>
+    <li><a href="https://google.com">button2</a></li>
+    <li><a href="https://google.com">button3</a></li>
+    <li><a href="https://google.com">button4</a></li>
   </ul>
 )
 
