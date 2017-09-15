7bac81160512f4602ca2a95ce3728f0ec5e310a0
angus.chen[陳志源] <angus.chen@104.com.tw>
2017-09-15 17:03:23 +0800

    fix: wrong component path & edit travis

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Navbar from './Navbar'
-import Footer from './Footer'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Navbar />, document.getElementById('nav'));
